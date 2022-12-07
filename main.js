fd50328284c96daa0392a57bcf34b5d84fcbfe1f
Nicolas Capdevielle <32545380+nn11kk00@users.noreply.github.com>
2022-12-07 17:43:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -23,8 +23,20 @@ const words = [
 let palabraAleatoria = "";
 let time = 10;
 let score = 0;
-let randomWord = document.querySelector("#randomWord")
+let scoreHTML = document.querySelector("#score");
+let randomWord = document.querySelector("#randomWord");
+let timeSpan = document.querySelector("#timeSpan");
+let timeInterval = setInterval(actualizarTiempo, 1000);
 
+
+document.querySelector("input").addEventListener('keypress', function (e) {
+  let palabraIngresada = "";
+  if (palabraIngresada == palabraAleatoria) {
+    time += 3;
+    input = "";
+  } else { addToDOM(); }
+  console.log(e);
+})
 
 function randomWords() {
   let i = parseInt(Math.random() * 18)
@@ -32,13 +44,25 @@ function randomWords() {
 }
 
 function addToDOM() {
-
   randomWord.textContent = palabraAleatoria;
-
 }
 
+function actualizarTiempo() {
+  if (time === 0) {
+    clearInterval(timeInterval)
+  } else {
+    time -= 1;
+    timeSpan.textContent = time + "s";
+  }
+}
 
+function updateScore() {
+  score += 1;
+  scoreHTML.textContent = score;
+}
 
 randomWords();
-addToDOM();
+actualizarTiempo();
+updateScore();
+
 console.log(palabraAleatoria);
